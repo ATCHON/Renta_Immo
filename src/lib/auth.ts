bbd6733b944d92fa6e1be3454d4012d71d36df62
AAlban <alban.atchon@gmail.com>
2026-02-04 23:16:55 +0100

    fix(auth) connexion à la bdd

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,9 @@ import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
-    database: {
-        db: new Pool({
-            connectionString: process.env.DATABASE_URL,
-        }),
-        type: "postgres",
-    },
+    database: new Pool({
+        connectionString: process.env.DATABASE_URL,
+    }),
     emailAndPassword: {
         enabled: true,
     },
