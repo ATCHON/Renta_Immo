ed66c5eb576ecd498f18e855d37c168b061c1fe6
AAlban <alban.atchon@gmail.com>
2026-02-05 22:13:46 +0100

    fix(auth) log variable database url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,10 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined"); // Fail fast if env var is missing
+}
+
 export const auth = betterAuth({
     database: new Pool({
         connectionString: process.env.DATABASE_URL,
